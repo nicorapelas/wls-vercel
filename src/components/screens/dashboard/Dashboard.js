84e721beffe0552b0ae9b3fccc09aa8f605dac8c
Nico Rapelas <nicorapelas@gmail.com>
2025-01-06 15:41:07 +0200

    added margin to dashbourd buttons

--- src/components/screens/dashboard/Dashboard.js
+++ src/components/screens/dashboard/Dashboard.js
@@ -32,12 +32,16 @@ const Dashboard = () => {
     return (
       <div className="dashboard-container">
         <div className="dashboard-buttons-container">
-          <button className="nav-button" onClick={handleMyCardsPress}>
-            My Cards
-          </button>
-          <button className="signout-button" onClick={handleSignout}>
-            Sign Out
-          </button>
+          <div className="dashboard-buttons-container-left">
+            <button className="nav-button" onClick={handleMyCardsPress}>
+              My Cards
+            </button>
+          </div>
+          <div className="dashboard-buttons-container-right">
+            <button className="signout-button" onClick={handleSignout}>
+              Sign Out
+            </button>
+          </div>
         </div>
         <main className="dashboard-content">
           <WP001 />
