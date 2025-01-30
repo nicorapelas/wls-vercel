8217b3b32f9e45f931eacbe7cb8f34a24bfb6ff4
Nico Rapelas <nicorapelas@gmail.com>
2025-01-30 16:34:31 +0200

    jo

--- src/api/ngrok.js
+++ src/api/ngrok.js
@@ -4,8 +4,12 @@ const instance = axios.create({
   baseURL: 'https://coups-1889de9f2619.herokuapp.com',
 })
 
+console.log('API Base URL:', instance.defaults.baseURL)
+
 instance.interceptors.request.use(
   async (config) => {
+    console.log('Request config:', config.baseURL)
+
     const token = localStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
